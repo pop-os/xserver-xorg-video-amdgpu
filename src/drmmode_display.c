pop-os/xserver-xorg-video-amdgpu

commit ba2aa0a8c12a2bea1e8be01ca3134b518d4cb0f2

Handle rotation in the driver also with Xorg 1.12-1.18
We cannot use the HW cursor in that case, but in turn we get more
efficient and less teary updates of rotated outputs.

(Ported from radeon commit f2bc882f1c1082bed9f496cfab6c8f07a76bc122)
Reviewed-by: Alex Deucher <alexander.deucher@amd.com>

src/drmmode_display.c
@@ -589,22 +589,20 @@ drmmode_can_use_hw_cursor(xf86CrtcPtr crtc)
 
 #if XF86_CRTC_VERSION >= 4
 
+#if XF86_CRTC_VERSION < 7
+#define XF86DriverTransformOutput TRUE
+#define XF86DriverTransformNone FALSE
+#endif
+
 static Bool
 drmmode_handle_transform(xf86CrtcPtr crtc)
 {
 	Bool ret;
 
-#if XF86_CRTC_VERSION >= 7
 	if (crtc->transformPresent || crtc->rotation != RR_Rotate_0)
 	    crtc->driverIsPerformingTransform = XF86DriverTransformOutput;
 	else
 	    crtc->driverIsPerformingTransform = XF86DriverTransformNone;
-#else
-	AMDGPUInfoPtr info = AMDGPUPTR(crtc->scrn);
-
-	crtc->driverIsPerformingTransform = crtc->transformPresent ||
-		(info->tear_free && crtc->rotation != RR_Rotate_0);
-#endif
 
 	ret = xf86CrtcRotate(crtc);
 

commit 35106fc0a948957cbb7e1e9649c89993a3d5c95c

Only handle reflection in the driver with Xorg < 1.16
Xorg doesn't handle the hardware cursor correctly in that case for
rotation and general transforms, and we can't force the SW cursor.

Fixes: ba2aa0a8c12a ("Handle rotation in the driver also with Xorg
                      1.12-1.18")
(Cherry picked from radeon commit 7d7abf99b5441ddb04dbee99bc8fa7abc30d4c46)
Reviewed-by: Alex Deucher <alexander.deucher@amd.com>

src/drmmode_display.c
@@ -615,10 +615,16 @@ drmmode_handle_transform(xf86CrtcPtr crtc)
 {
 	Bool ret;
 
+#if XORG_VERSION_CURRENT >= XORG_VERSION_NUMERIC(1,15,99,903,0)
 	if (crtc->transformPresent || crtc->rotation != RR_Rotate_0)
 	    crtc->driverIsPerformingTransform = XF86DriverTransformOutput;
 	else
 	    crtc->driverIsPerformingTransform = XF86DriverTransformNone;
+#else
+	crtc->driverIsPerformingTransform = !crtc->transformPresent &&
+		crtc->rotation != RR_Rotate_0 &&
+		(crtc->rotation & 0xf) == RR_Rotate_0;
+#endif
 
 	ret = xf86CrtcRotate(crtc);
 