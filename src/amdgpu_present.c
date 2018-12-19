pop-os/xserver-xorg-video-amdgpu

commit 2d18b37159edc526c73a36143fe9b5d6b75e610a

Check last flip window instead of screen root before flipping
A significant amount of time can pass between the X call into
check_flip for a window and when amdgpu_present_flip actually occurs.
To ensure that flipping is still possible there was an additional check
performed on screen->root in amdgpu_present_flip - but what should
be checked instead is the window itself. This only really worked before
because X ensures that the window has the same dimensions as the screen
to allow for present extension flipping.

This patch tracks the flip window between calls to check_flip and flip
and uses that window instead of screen->root.

Signed-off-by: Nicholas Kazlauskas <nicholas.kazlauskas@amd.com>

src/amdgpu_present.c
@@ -273,6 +273,8 @@ amdgpu_present_check_flip(RRCrtcPtr crtc, WindowPtr window, PixmapPtr pixmap,
 	if (num_crtcs_on == 0)
 		return FALSE;
 
+	info->flip_window = window;
+
 	return TRUE;
 }
 
@@ -313,13 +315,12 @@ amdgpu_present_flip(RRCrtcPtr crtc, uint64_t event_id, uint64_t target_msc,
                    PixmapPtr pixmap, Bool sync_flip)
 {
 	xf86CrtcPtr xf86_crtc = crtc->devPrivate;
-	ScreenPtr screen = crtc->pScreen;
 	ScrnInfoPtr scrn = xf86_crtc->scrn;
 	AMDGPUInfoPtr info = AMDGPUPTR(scrn);
 	struct amdgpu_present_vblank_event *event;
 	Bool ret = FALSE;
 
-	if (!amdgpu_present_check_flip(crtc, screen->root, pixmap, sync_flip))
+	if (!amdgpu_present_check_flip(crtc, info->flip_window , pixmap, sync_flip))
 		return ret;
 
 	event = calloc(1, sizeof(struct amdgpu_present_vblank_event));
@@ -359,6 +360,8 @@ amdgpu_present_unflip(ScreenPtr screen, uint64_t event_id)
 		(amdgpu_present_screen_info.capabilities & PresentCapabilityAsync) ?
 		FLIP_ASYNC : FLIP_VSYNC;
 	int i;
+
+	info->flip_window = NULL;
 
 	if (!amdgpu_present_check_unflip(scrn))
 		goto modeset;

src/amdgpu_drv.h
@@ -301,6 +301,7 @@ typedef struct {
 	int group_bytes;
 
 	/* kms pageflipping */
+	WindowPtr flip_window;
 	Bool allowPageFlip;
 
 	/* cursor size */

commit 233a0be82d5c317e58002f4daf836d4f95048465

Don't clear info->flip_window in present_unflip
present_unflip can get called between present_check_flip and
present_flip, in which case the latter would pass a NULL WindowPtr to
the former, resulting in a crash.

present_flip should never get called for a window which has already been
destroyed, so there's no need to clear info->flip_window.

Bugzilla: https://bugs.freedesktop.org/109067
Fixes: 2d18b37159edc "Check last flip window instead of screen root
                      before flipping"
Reviewed-by: Alex Deucher <alexander.deucher@amd.com>

src/amdgpu_present.c
@@ -389,7 +389,6 @@ amdgpu_present_unflip(ScreenPtr screen, uint64_t event_id)
 		FLIP_ASYNC : FLIP_VSYNC;
 	int i;
 
-	info->flip_window = NULL;
 	info->drmmode.vrr_flipping = FALSE;
 	amdgpu_present_set_screen_vrr(scrn);
 